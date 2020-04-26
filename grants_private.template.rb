5e65b81e43ea9455bf5f785aba8bf3427fb6d74c
Rajesh Kumar <meetrajesh@gmail.com>
2020-04-26 09:47:23 -0700

    update template

--- grants_private.template.rb
+++ grants_private.template.rb
@@ -6,11 +6,11 @@ GRANTS = [
   StockOptionGrant.new(type: StockOptionGrant::OPTION_TYPES.nso, strike: 1.79, num_options: 12_941),
   StockOptionGrant.new(type: StockOptionGrant::OPTION_TYPES.nso, strike: 1.82, num_options: 2_250),
   StockOptionGrant.new(type: StockOptionGrant::OPTION_TYPES.nso, strike: 2.45, num_options: 245),
-  StockOptionGrant.new(type: StockOptionGrant::OPTION_TYPES.nso, strike: 4.55, num_options: 3_535),
+  StockOptionGrant.new(type: StockOptionGrant::OPTION_TYPES.nso, strike: 4.55, num_options: 4_650),
   
   # ISOs vested in your final year
   StockOptionGrant.new(type: StockOptionGrant::OPTION_TYPES.iso, strike: 7.65, num_options: 9_535, num_flipped_rightaway: 1_500),
-  StockOptionGrant.new(type: StockOptionGrant::OPTION_TYPES.iso, strike: 10.43, num_options: 405),
+  StockOptionGrant.new(type: StockOptionGrant::OPTION_TYPES.iso, strike: 10.43, num_options: 4_698),
   
   # RSUs vested in your final year
   StockOptionGrant.new(type: StockOptionGrant::OPTION_TYPES.rsu, strike: 0, num_options: 0), # none vested
